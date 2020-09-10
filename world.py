23f8c69d4d9534a1dd1a9ff3cf08f64f393f07fa
hy00nc <chohy0555@gmail.com>
2020-09-10 18:58:35 +0900

    fix bye_world

--- world.py
+++ world.py
@@ -5,9 +5,9 @@ def hello_world():
     print("hello world")
 
 def bye_world():
+    time.sleep(2)
     print("bye world")
 
 if __name__ == "__main__":
     hello_world()
-    time.sleep(2)
     bye_world()
